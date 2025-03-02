7074c7c08a957637b5dfce97434cc77073065a16
pradip983 <vasanpradip06@gmail.com>
2025-03-02 18:54:28 +0530

    Update userService.js
    change

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;  // Make sure this matches your backend
+const API_URL = "http://localhost:5000/api/users";  // Make sure this matches your backend
 
 export const registerUserCustomer = async (userData) => {
     try {
