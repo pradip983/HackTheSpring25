aafc45f22ef7d27c739ff09ff3b5ff4f37e2a88e
pradip983 <vasanpradip06@gmail.com>
2025-03-03 11:57:51 +0530

    Update userService.js
    change url

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://hack-the-spring25-sq65.vercel.app/api/users";  // Make sure this matches your backend
+const API_URL = "https://expanding-express.vercel.app/api/users";  // Make sure this matches your backend
 
 export const registerUserCustomer = async (userData) => {
     try {
